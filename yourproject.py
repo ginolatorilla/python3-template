8af18ef64b3ddcde818e37934c5d08ceed860a68
Gino Latorilla <pblatorilla+github@gmail.com>
2019-04-08 00:15:40 +0800

    Added main program with argparse

---
+++ yourproject.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+'''Describe in this sentence what this program does.
+
+Copyright (c) 2019 Gino Latorilla.
+'''
+
+import argparse
+
+
+def main():
+    program_options = get_program_options()
+    x = yourproject.submodule.yourfunction()
+    # Continue here
+
+
+def get_program_options():
+    parser = argparse.ArgumentParser(description=__doc__)
+    return parser.parse_args()
+
+
+if __name__ == '__main__':
+    main()
