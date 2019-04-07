9620616d741c2cd05932f7047985db46b6ca085b
Gino Latorilla <pblatorilla+github@gmail.com>
2019-04-07 23:31:01 +0800

    Added bootstrapper that initialises pipenv

---
+++ bootstrap.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+'''
+Copyright (c) 2019 Gino Latorilla.
+'''
+
+import subprocess
+import os
+
+env = os.environ.copy()
+env['PIPENV_VENV_IN_PROJECT'] = '1'
+subprocess.run('pipenv --python 3 --three'.split(), env=env)
