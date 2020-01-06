74bb14842d50294ab91af22c492dcc5abd58294d
Gino Latorilla <pblatorilla+github@gmail.com>
2020-01-06 21:35:21 +0800

    Install all packages with bootstrapper

--- bootstrap.py
+++ bootstrap.py
@@ -10,3 +10,4 @@ import os
 env = os.environ.copy()
 env['PIPENV_VENV_IN_PROJECT'] = '1'
 subprocess.run('pipenv --python 3 --three'.split(), env=env)
+subprocess.run('pipenv install --dev'.split())
