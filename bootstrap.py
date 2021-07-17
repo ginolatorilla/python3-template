38baa2a1a7a941bcb29276c8433a74a55f9f9d51
Gino Latorilla <pblatorilla+github@gmail.com>
2021-07-17 17:41:30 +0800

    In bootstrap, install project as editable package

--- bootstrap.py
+++ bootstrap.py
@@ -6,8 +6,9 @@ Copyright (c) 2019 Gino Latorilla. All rights reserved.
 
 import subprocess
 import os
+import shlex
 
 env = os.environ.copy()
 env['PIPENV_VENV_IN_PROJECT'] = '1'
-subprocess.run('pipenv --python 3 --three'.split(), env=env)
-subprocess.run('pipenv install --dev'.split())
+subprocess.run(shlex.split('pipenv --python 3 --three'), env=env)
+subprocess.run(shlex.split('pipenv install --editable . --dev'), env=env)
