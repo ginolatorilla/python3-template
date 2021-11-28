46b8b62a373e7d304d1e2a9b6a9f79174458586e
Gino Latorilla <pblatorilla+github@gmail.com>
2021-11-28 11:46:45 +0800

    Remove myself from the copyrights

--- submodule/__init__.py
+++ submodule/__init__.py
@@ -1,3 +1,8 @@
-"""
-Copyright (c) 2019 Gino Latorilla. All rights reserved.
-"""
+'''This module has some stuff in it.
+This module can do this and that, and many many more.
+If you think there's no need to describe this in detail, just remove this line and the one before.
+
+Copyright (c) 2021 Your Company's Name, or you
+'''
+
+# Intentionally left blank.
--- tests/yourproject_test.py
+++ tests/yourproject_test.py
@@ -1,6 +1,8 @@
-"""
-Copyright (c) 2019 Gino Latorilla. All rights reserved.
-"""
+'''This is a test.
+If you think there's no need to describe this in detail, just remove this line, or this entire docstring.
+
+Copyright (c) 2021 Your Company's Name, or you
+'''
 
 import pytest
 import yourproject
