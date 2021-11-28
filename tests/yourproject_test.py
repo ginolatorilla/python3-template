2410814c9bf6a5dcfc3c2235601f8c3a3c74ffb3
Gino Latorilla <pblatorilla+github@gmail.com>
2021-11-28 13:52:57 +0800

    CLI Templates (#3)
    * Yourproject converted to a sibling module
    
    * Added CLI template with logging support
    
    * Licence under PyPI
    
    * Register CLI as a console script
    
    * Remove myself from the copyrights
    
    * Fix tests for yourproject.main after adding arguments
    
    * More package metadata
    
    * Remove unused trailing if == '__main__' in cli.py
    
    * Use dynamic type creation with formatter class
    
    * Add subcommands support
    
    * Do not show full file path in logs
    
    * Support for coloured CLI
    
    * Document new CLI template and optional features
    
    * Increase linter line length to 119
    
    * Fix pycodestyle findings
    
    * Support up to Python 3.10, but drop 3.6 for Macs
    
    * Run workflows for the main branch and pull requests to main
    
    * As in previous commit, for all OSes
    
    * Increase testing range across all OSes

--- tests/yourproject_test.py
+++ tests/yourproject_test.py
@@ -1,6 +1,8 @@
-"""
-Copyright (c) 2019 Gino Latorilla. All rights reserved.
-"""
+'''This is a test.
+If you think there's no need to describe this in detail, just remove this line, or this entire docstring.
+
+Copyright (c) 2021 Your Company's Name, or you
+'''
 
 import pytest
 import yourproject
@@ -9,7 +11,7 @@ import submodule.main
 
 
 def test_yourproject_main():
-    assert yourproject.main() == 0
+    assert yourproject.main(None) is None
 
 
 def test_yourfunction():
